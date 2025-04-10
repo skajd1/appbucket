d4b7bc159d23ef5205bb87c0e6f219358271f431
skajd1 <woochul102@gmail.com>
2025-04-11 07:26:38 +0900

    fix: 앱 중복 제거

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -33,7 +33,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'users',
     'bucket',
     'rest_framework',
